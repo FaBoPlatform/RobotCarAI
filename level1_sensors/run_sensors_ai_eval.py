21296d7ad9fbe0e4a21a02e380abe8deee2e748c
Yoshiroh Takanashi <takanashi@gclue.jp>
2018-01-09 20:02:40 +0900

    update level1

--- level1_sensors/run_sensors_ai_eval.py
+++ level1_sensors/run_sensors_ai_eval.py
@@ -1,5 +1,6 @@
 # coding: utf-8
-# 予測精度を評価する
+# センサー値を取得し、予測を実行する
+# ジェネレータの結果と比較し、精度を評価する
 
 import time
 import logging
@@ -21,125 +22,129 @@ logging.basicConfig(level=logging.DEBUG,
 )
 
 
-'''
-予測の精度評価を行う
-'''
+def print_log(sensors,ai,ai_value,if_value,counter,miss_counter,bad_score_counter,keep=False):
+    '''
+    コンソールに表示する
+    '''
+    ########################################
+    # 予測結果を文字列に変換する
+    ########################################
+    if ai_value == 0:
+        result = 'STOP'
+    elif ai_value == 1:
+        result = 'LEFT'
+    elif ai_value == 2:
+        result = 'FORWARD'
+    elif ai_value == 3:
+        result = 'RIGHT'
+
+    # 予測結果のスコアが低い時、予測結果の文字列にBAD SCOREを入れる
+    if ai_value == ai.get_other_label():
+        result = 'BAD SCORE'
+
+    # 予測結果とジェネレータ結果が同じならok、異なるならngを付ける
+    if if_value == ai_value:
+        suffix = 'ok'
+    else:
+        suffix = 'ng'
+        if if_value == 0:
+            suffix += ' generator:STOP'
+        elif if_value == 1:
+            suffix += ' generator:LEFT'
+        elif if_value == 2:
+            suffix += ' generator:FORWARD'
+        elif if_value == 3:
+            suffix += ' generator:RIGHT'
+
+    sys.stdout.write("accuracy:"+str((counter-miss_counter)/(counter*1.0))+" total:"+str(counter)+" miss:"+str(miss_counter)+" bad score:"+str(bad_score_counter)+" result:"+result+" "+str(sensors)+" - "+suffix+"                \r")
+    sys.stdout.flush()
+    if keep:
+        print("")
+        sys.stdout.flush()
+
+    return
+
+
 def main():
+    '''
+    予測を実行する
+    '''
     # AI準備
     ai = AI()
-    score = 0.0 # スコア閾値
+    # スコア閾値。予測結果がこれより低いスコアの時はその他と見なす。
+    score = 0.6
 
     # IF準備 (AI学習データジェネレータ)
     generator = SensorGenerator()
-    # 近接センサー準備
-    kerberos = Kerberos()
-    LIDAR_INTERVAL = 0.05
 
+    # 評価した回数をカウント
+    counter = 0
+    # 低スコアの回数をカウント
+    bad_score_counter = 0
+    # 予測結果とジェネレータ結果が異なる回数をカウント(低スコアの回数も含む)
+    miss_counter = 0
+    # 評価する距離範囲
+    min_range = 0
+    max_range = 200
     try:
         learned_step = ai.get_learned_step()
         print("learned_step:{}".format(learned_step))
 
         ########################################
-        # N回予測を実行し、IF文との不一致数をカウントする
+        # 近接センサー値を生成する
         ########################################
-        N=1000
-        for i in range(0,N):
-            ########################################
-            # 近接センサー値を取得する
-            ########################################
-            '''
-            速度、角度制御を入れるので距離はここで取る
-            '''
-            distance1,distance2,distance3 = kerberos.get_distance()
-            sensors = [distance1,distance2,distance3]
-            ########################################
-            # AI予測結果を取得する
-            ########################################
-            # 今回の予測結果を取得する
-            ai_value = ai.get_prediction(sensors,score)
-            ########################################
-            # IF結果を取得する
-            ########################################
-            # 今回の結果を取得する
-            w = generator.driving_instruction(sensors)
-            if_value = np.argmax(w[0:4])
+        for distance1 in range(min_range,max_range):
+            for distance2 in range(min_range,max_range):
+                sensors=[]
+                for distance3 in range(min_range,max_range):
+                    sensors = [distance1,distance2,distance3]
+                    counter +=1
 
+                    ########################################
+                    # AI予測結果を取得する
+                    ########################################
+                    # 今回の予測結果を取得する
+                    ai_value = ai.get_prediction(sensors,score)
 
-            print("ai_value:{} {}".format(ai_value,sensors))
-            # 予測結果のスコアが低い時は何もしない
-            if ai_value == ai.get_other_label():
-                time.sleep(LIDAR_INTERVAL)
-                continue
+                    # 予測結果のスコアが低かった回数をカウントする
+                    if ai_value == ai.get_other_label():
+                        bad_score_counter += 1
 
-            ########################################
-            # 速度調整を行う
-            ########################################
-            if distance2 >= 100:
-                speed = 100
-            else:
-                speed = int(distance2 + (100 - distance2)/2)
-                if speed < 40:
-                    speed = 40
+                    ########################################
+                    # IF結果を取得する
+                    ########################################
+                    # 今回の結果を取得する
+                    w = generator.driving_instruction(sensors)
+                    if_value = np.argmax(w[0:4])
 
-            ########################################
-            # ハンドル角調整を行う
-            ########################################
-            if ai_value == 1: # 左に行くけど、左右スペース比で舵角を制御する
-                ratio = float(distance1)/(distance1 + distance3) # 角度をパーセント減にする
-                if distance2 < 75 or distance3 < 8.0 :
-                    ratio = 1.0
-            elif ai_value == 3: # 右に行くけど、左右スペース比で舵角を制御する
-                ratio = float(distance3)/(distance1 + distance3) # 角度をパーセント減にする
-                if distance2 < 75 or distance1 < 8.0 :
-                    ratio = 1.0
-            else:
-                ratio = 1.0
+                    # 予測結果とジェネレータ結果が異なった回数をカウントする
+                    if not if_value == ai_value:
+                        miss_counter += 1
+                        print_log(sensors,ai,ai_value,if_value,counter,miss_counter,bad_score_counter,True)
 
-            if not FORCE_STOP_THREAD_RUN: break # 強制停止ならループを抜ける
-            ########################################
-            # ロボットカーを 前進、左右、停止 する
-            ########################################
-            if ai_value == STOP:
-                car.stop()
-                car.set_angle(HANDLE_NEUTRAL)
-            elif ai_value == LEFT:
-                car.set_angle(HANDLE_NEUTRAL - (HANDLE_ANGLE * ratio))
-                car.forward(speed)
-            elif ai_value == FORWARD:
-                car.forward(speed)
-                car.set_angle(HANDLE_NEUTRAL)
-            elif ai_value == RIGHT:
-                car.set_angle(HANDLE_NEUTRAL + (HANDLE_ANGLE * ratio))
-                car.forward(speed)
-
-            else:
-                if not FORCE_STOP_THREAD_RUN: break # 強制停止ならループを抜ける
-                count_stop = 0
-                # 前進の時は直前のハンドル操作を記憶する
-                qsize = log_queue.qsize()
-                if qsize >= max_log_length:
-                    log_queue.get(block=False)
-                    qsize = log_queue.qsize()
-                log_queue.put(ai_value)
-
-            time.sleep(LIDAR_INTERVAL)
+                    ########################################
+                    # 予測結果をコンソールに表示する
+                    ########################################
+                    if counter % 1000 == 0:
+                        print_log(sensors,ai,ai_value,if_value,counter,miss_counter,bad_score_counter)
 
     except:
         import traceback
         traceback.print_exc()
         print('error! main failed.')
     finally:
+        sys.stdout.write("accuracy:"+str((counter-miss_counter)/(counter*1.0))+" total:"+str(counter)+" miss:"+str(miss_counter)+" bad score:"+str(bad_score_counter)+"                \r")
+        sys.stdout.flush()
+        print("")
+        sys.stdout.flush()
+
         print("main end")
-        # ボタンスレッドを停止させる
-        FORCE_STOP_THREAD_RUN = False
-        car.stop()
-        car.set_angle(HANDLE_NEUTRAL)
         pass
 
     return
 
 if __name__ == '__main__':
-    # 停止ボタンの状態を監視するスレッドを起動する
-    t = threading.Thread(target=do_force_stop_button,args=())
-    t.start()
     main()
+
+
+
