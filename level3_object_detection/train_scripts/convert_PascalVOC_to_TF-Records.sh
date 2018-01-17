2d857d434dd3a270fcf7edaf933ad65f7293652d
Yoshiroh Takanashi <takanashi@gclue.jp>
2018-01-17 13:22:54 +0900

    update level3

--- level3_object_detection/train_scripts/convert_PascalVOC_to_TF-Records.sh
+++ level3_object_detection/train_scripts/convert_PascalVOC_to_TF-Records.sh
@@ -9,11 +9,13 @@ source $SCRIPT_DIR/../script_define.conf
 ####################
 OUTPUT_NAME=${MY_TRAIN}_train
 
+
 #OUTPUT_NAMEは読み込み時にSSD-TensorFlowのどこかにハードコーディングされている
 # find ./ -type f -name "*.py" | xargs grep -Hn -B 0 -A 0 "voc_2007_" 2> /dev/null
 #./datasets/pascalvoc_2007.py:22:FILE_PATTERN = 'voc_2007_%s_*.tfrecord'
 #voc_2007_train_*.tfrecord
 
+mkdir -p $TF_DATASET_DIR
 
 cd $SSD_TENSORFLOW_DIR
 
