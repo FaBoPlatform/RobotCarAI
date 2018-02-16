8963dab4ae296859b4cb43f75f257453c0942fcd
Yoshiroh Takanashi <takanashi@gclue.jp>
2018-02-16 15:19:01 +0900

    update level3

--- level3_object_detection/install_scripts/patch_to.sh
+++ level3_object_detection/install_scripts/patch_to.sh
@@ -11,9 +11,9 @@ source $SCRIPT_DIR/../script_define.conf
 if [ ! -e $SSD_TENSORFLOW_DIR/preprocessing/ssd_vgg_preprocessing.py.org ]; then
     cp $SSD_TENSORFLOW_DIR/preprocessing/ssd_vgg_preprocessing.py $SSD_TENSORFLOW_DIR/preprocessing/ssd_vgg_preprocessing.py.org
 fi
-patch -u $SSD_TENSORFLOW_DIR/preprocessing/ssd_vgg_preprocessing.py <  $SCRIPT_DIR/../patch_to_SSD-Tensorflow/ssd_vgg_preprocessing.patch
+patch -u $SSD_TENSORFLOW_DIR/preprocessing/ssd_vgg_preprocessing.py < $SCRIPT_DIR/../patch_to_SSD-Tensorflow/ssd_vgg_preprocessing.patch
 
 if [ ! -e $SSD_TENSORFLOW_DIR/nets/ssd_vgg_300.py.org ]; then
     cp $SSD_TENSORFLOW_DIR/nets/ssd_vgg_300.py $SSD_TENSORFLOW_DIR/nets/ssd_vgg_300.py.org
 fi
-patch -u $SSD_TENSORFLOW_DIR/nets/ssd_vgg_300.py <  $SCRIPT_DIR/../patch_to_SSD-Tensorflow/ssd_vgg_300.patch
+patch -u $SSD_TENSORFLOW_DIR/nets/ssd_vgg_300.py < $SCRIPT_DIR/../patch_to_SSD-Tensorflow/ssd_vgg_300.patch
